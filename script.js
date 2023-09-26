6846665ff9d353b46af6e1ca3d26763311ef1b33
Joseph <qjlu@ucsd.edu>
2023-09-25 21:42:06 -0700

    add more comments for better understanding and remove console log

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ containers.forEach((container) => {
     e.preventDefault();
     //get the element that your mouse is hovering on (aka about to drop it on to)
     const afterElement = getDragAfterElement(container, e.clientY);
-    console.log(afterElement);
     //select dragged element since only 1 can exists at a time
     const draggble = document.querySelector(".dragging");
     if (afterElement == null) {
@@ -47,8 +46,12 @@ const getDragAfterElement = (container, y) => {
   // for each elements that's not draggable find the closest element that is near mouse hover and return the element
   return draggableElements.reduce(
     (closest, child) => {
+      //get box boundries
       const box = child.getBoundingClientRect();
+      //get offset which is the different between your mouse and the middle point of the current container
       const offset = y - box.top - box.height / 2;
+      //less than zero means we are above the middle point of the container
+      //since going above middle point will result is lesser value we want the closest point to the mid
       if (offset < 0 && offset > closest.offset) {
         return { offset: offset, element: child };
       } else {
