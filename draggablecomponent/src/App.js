cc769cc6b431a36ee0601886b1e066eab1666b88
Joseph <qjlu@ucsd.edu>
2023-09-26 20:54:40 -0700

    WIP

--- draggablecomponent/src/App.js
+++ draggablecomponent/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useRef } from "react";
+import "./App.css";
+
+const ArrayElements = ({ ele, selectedEle }) => {
+  const eleRef = useRef();
+  const [isSelect, setIsSelected] = useState(false);
+  const style = isSelect ? { opacity: 0.5 } : { opacity: 1 };
+
+  const dragTrigger = () => {
+    if (isSelect) {
+      selectedEle.current = null;
+    } else {
+      selectedEle.current = ele;
+    }
+    setIsSelected(!isSelect);
+  };
+
+  return (
+    <>
+      <p
+        ref={eleRef}
+        style={style}
+        onDragStart={dragTrigger}
+        onDragEnd={dragTrigger}
+        className="draggable"
+        draggable
+      >
+        {ele}
+      </p>
+    </>
+  );
+};
 
 function App() {
+  const [draggableArray1, setDraggableArray1] = useState(["1", "2"]);
+  const [draggableArray2, setDraggableArray2] = useState(["3", "4"]);
+  const selectedEle = useRef(null);
+
+  const dragOver = (e) => {
+    e.preventDefault();
+    const className = e.target.className;
+    const arrayMoveTo = !draggableArray1.includes(selectedEle.current)
+      ? draggableArray1
+      : draggableArray2;
+    const arrayMoveFrom = draggableArray1.includes(selectedEle.current)
+      ? draggableArray2
+      : draggableArray1;
+
+    if (className.includes("top")) {
+      console.log("HI??");
+    } else {
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div onDragOver={dragOver} className="container top">
+        {draggableArray1.map((ele, index) => (
+          <ArrayElements selectedEle={selectedEle} key={index} ele={ele} />
+        ))}
+      </div>
+      <div onDragOver={dragOver} className="container bot">
+        {draggableArray2.map((ele, index) => (
+          <ArrayElements selectedEle={selectedEle} key={index} ele={ele} />
+        ))}
+      </div>
     </div>
   );
 }
