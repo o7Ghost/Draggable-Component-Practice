2e9ab2857a5a31f02acb3e8894e21bbcc6690e72
Joseph <qjlu@ucsd.edu>
2023-10-01 16:04:35 -0700

    add sortable drag components

--- draggablecomponent/src/App.js
+++ draggablecomponent/src/App.js
@@ -7,12 +7,10 @@ const ArrayElements = ({ ele, selectedEle, currSelect }) => {
 
   const dragStart = () => {
     selectedEle(ele);
-    console.log("dragStart?", ele);
   };
 
   const dragEnd = () => {
     selectedEle(null);
-    console.log("end?", ele);
   };
 
   return (
@@ -22,7 +20,7 @@ const ArrayElements = ({ ele, selectedEle, currSelect }) => {
         style={style}
         onDragStart={dragStart}
         onDragEnd={dragEnd}
-        className="draggable"
+        className={`draggable ${ele}`}
         draggable
       >
         {ele}
@@ -36,29 +34,130 @@ function App() {
   const [draggableArray2, setDraggableArray2] = useState(["c", "d"]);
   const [selectedEle, setSelectedEle] = useState(null);
 
+  const insertTop = (
+    insertAfter,
+    elementInsertInto,
+    arrayCopyTo,
+    setArrayCopyTo,
+    setDeleteEleFrom,
+    arrayDelFrom
+  ) => {
+    const newArray = [...arrayCopyTo];
+
+    if (arrayCopyTo.includes(selectedEle)) {
+      const originalIndex = arrayCopyTo.indexOf(selectedEle);
+      newArray.splice(originalIndex, 1);
+    } else {
+      setDeleteEleFrom(
+        arrayDelFrom.filter((element) => element !== selectedEle)
+      );
+    }
+    const idx = newArray.indexOf(elementInsertInto);
+    newArray.splice(idx + insertAfter, 0, selectedEle);
+    setArrayCopyTo(newArray);
+  };
+
   const dragOver = (e) => {
     e.preventDefault();
     const className = e.target.className;
-    const arrayMoveTo = !draggableArray1.includes(selectedEle)
-      ? draggableArray1
-      : draggableArray2;
-    const arrayMoveFrom = draggableArray1.includes(selectedEle)
-      ? draggableArray1
-      : draggableArray2;
 
-    if (className.includes("top")) {
-      if (!draggableArray1.includes(selectedEle)) {
-        setDraggableArray2(
-          arrayMoveFrom.filter((element) => element !== selectedEle)
-        );
-        setDraggableArray1([...arrayMoveTo, selectedEle]);
+    if (
+      className.includes("draggable") &&
+      className !== `draggable ${selectedEle}`
+    ) {
+      const container = e.target.parentNode.className;
+      const currentMousePosition = e.clientY;
+      const box = e.target.getBoundingClientRect();
+      const offset = currentMousePosition - box.top - box.height / 2;
+      const elementInsertInto = e.target.className.split(" ")[1];
+      if (offset < 0) {
+        if (container.includes("top")) {
+          const indexInsertTo = draggableArray1.indexOf(elementInsertInto);
+          if (indexInsertTo - 1 !== draggableArray1.indexOf(selectedEle)) {
+            insertTop(
+              0,
+              elementInsertInto,
+              draggableArray1,
+              setDraggableArray1,
+              setDraggableArray2,
+              draggableArray2
+            );
+          } else if (
+            indexInsertTo === 0 &&
+            indexInsertTo !== draggableArray1.indexOf(selectedEle)
+          ) {
+            insertTop(
+              0,
+              elementInsertInto,
+              draggableArray1,
+              setDraggableArray1,
+              setDraggableArray2,
+              draggableArray2
+            );
+          }
+        } else if (container.includes("bot")) {
+          const indexInsertTo = draggableArray2.indexOf(elementInsertInto);
+          if (indexInsertTo - 1 !== draggableArray2.indexOf(selectedEle)) {
+            insertTop(
+              0,
+              elementInsertInto,
+              draggableArray2,
+              setDraggableArray2,
+              setDraggableArray1,
+              draggableArray1
+            );
+          } else if (
+            indexInsertTo === 0 &&
+            indexInsertTo !== draggableArray2.indexOf(selectedEle)
+          ) {
+            insertTop(
+              0,
+              elementInsertInto,
+              draggableArray2,
+              setDraggableArray2,
+              setDraggableArray1,
+              draggableArray1
+            );
+          }
+        }
+      } else {
+        if (container.includes("top")) {
+          const indexInsertTo = draggableArray1.indexOf(elementInsertInto);
+          if (indexInsertTo + 1 !== draggableArray1.indexOf(selectedEle)) {
+            insertTop(
+              1,
+              elementInsertInto,
+              draggableArray1,
+              setDraggableArray1,
+              setDraggableArray2,
+              draggableArray2
+            );
+          }
+        } else if (container.includes("bot")) {
+          const indexInsertTo = draggableArray2.indexOf(elementInsertInto);
+          if (indexInsertTo + 1 !== draggableArray2.indexOf(selectedEle)) {
+            insertTop(
+              1,
+              elementInsertInto,
+              draggableArray2,
+              setDraggableArray2,
+              setDraggableArray1,
+              draggableArray1
+            );
+          }
+        }
       }
-    } else if (className.includes("bot")) {
-      if (!draggableArray2.includes(selectedEle)) {
-        setDraggableArray1(
-          arrayMoveFrom.filter((element) => element !== selectedEle)
+    } else {
+      if (className.includes("top") && draggableArray1.length === 0) {
+        setDraggableArray2(
+          draggableArray2.filter((element) => element !== selectedEle)
         );
-        setDraggableArray2([...arrayMoveTo, selectedEle]);
+        setDraggableArray1([...draggableArray1, selectedEle]);
+      } else if (className.includes("bot") && draggableArray2.length === 0) {
+        setDraggableArray1(
+          draggableArray1.filter((element) => element !== selectedEle)
+        );
+        setDraggableArray2([...draggableArray2, selectedEle]);
       }
     }
   };
